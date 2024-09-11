9f4657aab7e894c835afdc2c71f897266f3d4979
Matthew Lim <125940571+MatthewTLim@users.noreply.github.com>
2024-09-10 18:01:55 -0700

    Add validations for job model

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,10 @@
 class Job < ApplicationRecord
+  validates :company_name, presence: true, length: { maximum: 100 }
+  validates :post_title, presence: true
+  validates :job_category, presence: true
+  validates :part_time_full_time, presence: true
+  validates :location, presence: true, length: { maximum: 100 }
+  validates :job_post_date, presence: true
+  validates :apply_link, presence: true, format: { with: /\A#{URI::regexp(['http', 'https'])}\z/, message: "must be a valid URL" }
+  validates :details, length: { maximum: 1000 }, allow_blank: true
 end
